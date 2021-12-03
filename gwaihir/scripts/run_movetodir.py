8eb26a9d16839ea1f687f0cf9d4225e2f46af109
NoMachineESRF <david.simonne@synchrotron-soleil.fr>
2021-12-03 21:34:16 +0100

    edit run_movedir script

--- gwaihir/scripts/run_movetodir.py
+++ gwaihir/scripts/run_movetodir.py
@@ -14,83 +14,73 @@ import inspect
 
 # Print help
 try:
-    print('OG data dir:',  sys.argv[1])
-    print('Target data dir:',  sys.argv[2])
-    print('Scan (s):',  sys.argv[3])
+    print('Data dir:',  sys.argv[1])
+    print('Root folder:',  sys.argv[2])
+    print('Scan:',  sys.argv[3])
 except IndexError:
     print("""
         Arg 1: Original data directory 
-        Arg 2: Path of EXISTING target directory (e.g. Pt_Al2O3/) (subdirectories S{scan}/data & S{scan}/pynx_raw will be updated/created)
-        Arg 3: Scan(s) number, list or single value
+        Arg 2: Path of EXISTING target directory (e.g. Pt_Al2O3/) (subdirectories {scan}/data & {scan}/pynx_raw will be updated/created)
+        Arg 3: Scan number
 
         Looks recursively for one mu or omega scan including the scan number (glob.glob).
         """)
     exit()
 
 OG_folder = sys.argv[1]
-TG_folder = sys.argv[2]
-scan_list = sys.argv[3]
+root_folder = sys.argv[2]
+scan_name = sys.argv[3]
 
-# transform string of list into python list object
-if scan_list.startswith("["):
-    scans = ast.literal_eval(scan_list)
+# Assign scan folder
+scan_folder = root_folder + scan_name + "/"
+print("Scan folder:", scan_folder)
 
-else:
-    scans = [scan_list]
+# Assign preprocessing folder
+preprocessing_folder = scan_folder + "preprocessing/"
 
-# Load data
-for scan in scans:
-    print(f"Moving scan {scan}...")
-    try:
-        os.mkdir(f"{TG_folder}S{scan}")
-        print(f"Created {TG_folder}S{scan}")
-    except FileExistsError:
-        print(f"{TG_folder}S{scan} exists")
-        pass
+# Assign postprocessing folder
+postprocessing_folder = scan_folder + "postprocessing/"
 
-    try:
-        os.mkdir(f"{TG_folder}S{scan}/data")
-        print(f"Created {TG_folder}S{scan}/data")
-    except FileExistsError:
-        print(f"{TG_folder}S{scan}/data exists")
-        pass
+# Assign data folder
+data_folder = scan_folder + "data/"
 
-    try:
-        os.mkdir(f"{TG_folder}S{scan}/pynxraw")
-        print(f"Created {TG_folder}S{scan}/pynxraw")
-    except FileExistsError:
-        print(f"{TG_folder}S{scan}/pynxraw exists")
-        pass
-
-    try:
-        root_dir = inspect.getfile(gwaihir).split("__")[0]
-        shutil.copy(f"{root_dir}gwaihir/data_files/pynx_run.txt",
-                    f"{TG_folder}S{scan}/pynxraw")
-        print(f"Copied pynx-run-no-support.txt to {TG_folder}S{scan}/pynxraw")
-    except FileExistsError:
-        print(f"{TG_folder}S{scan}/pynxraw/pynx-run-no-support.txt exists")
-        pass
-
-    try:
-        filename = glob.glob(f"{OG_folder}*mu*{scan}*.nxs", recursive=True)[0]
-        print("Mu scan.")
-        shutil.copy2(filename, f"{TG_folder}S{scan}/data")
-        print(f"Copied {filename} to {TG_folder}S{scan}/data")
-    except FileExistsError:
-        print(f"{TG_folder}S{scan}/data/{filename} exists")
-        pass
-    except IndexError:
+# Create final directory, if not yet existing
+if not os.path.isdir(root_folder):
+    print(root_folder)
+    full_path = ""
+    for d in root_folder.split("/"):
+        full_path += d + "/"
         try:
-            filename = glob.glob(
-                f"{OG_folder}*omega*{scan}*.nxs", recursive=True)[0]
-            print("Omega scan.")
-            shutil.copy2(filename, f"{TG_folder}S{scan}/data")
-            print(f"Copied {filename} to {TG_folder}S{scan}/data")
+            os.mkdir(full_path)
         except FileExistsError:
-            print(f"{TG_folder}S{scan}/data/{filename} exists")
-            pass
-        except IndexError:
-            print("Not a mu or an omega scan.")
             pass
 
-    print("\n")
+print("Updating directories ...")
+
+# Scan directory
+try:
+    os.mkdir(f"{scan_folder}")
+    print(f"\tCreated {scan_folder}")
+except FileExistsError:
+    print(f"\t{scan_folder} exists")
+
+# /data directory
+try:
+    os.mkdir(f"{data_folder}")
+    print(f"\tCreated {data_folder}")
+except FileExistsError:
+    print(f"\t{data_folder} exists")
+
+# /preprocessing directory
+try:
+    os.mkdir(f"{preprocessing_folder}")
+    print(f"\tCreated {preprocessing_folder}")
+except FileExistsError:
+    print(f"\t{preprocessing_folder} exists")
+
+# /postprocessing directory
+try:
+    os.mkdir(f"{postprocessing_folder}")
+    print(f"\tCreated {postprocessing_folder}", end="\n\n")
+except FileExistsError:
+    print(f"\t{postprocessing_folder} exists", end="\n\n")
