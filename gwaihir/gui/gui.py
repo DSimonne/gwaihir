263b291d5c6111493986259898fce4c45bd2d2d3
SixS3 David <david.simonne@synchrotron-soleil.fr>
2022-07-01 20:18:48 +0200

    Add possibility for new detector at SixS, Merlin

--- gwaihir/gui/gui.py
+++ gwaihir/gui/gui.py
@@ -597,7 +597,7 @@ class Interface:
 
             detector=widgets.Dropdown(
                 options=[
-                    "Eiger2M", "Maxipix", "Eiger4M", "Merlin", "Timepix"],
+                    "Eiger2M", "Maxipix", "Eiger4M", "Merlin", "MerlinSixS", "Timepix"],
                 value="Merlin",
                 description='Detector',
                 continuous_update=False,
