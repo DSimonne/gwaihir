1c4c87896f142952c6df8cc4bcc85508166014b5
Ravenne <51970962+DSimonne@users.noreply.github.com>
2023-02-10 10:16:34 +0100

    Edit docs

--- gwaihir/controller/control_readme.py
+++ gwaihir/controller/control_readme.py
@@ -48,7 +48,9 @@ def init_readme_tab(contents):
         display(Markdown("## SixS data (SOLEIL)"))
         display(Markdown(
             "Most of the initial guesses are valid. Be careful about the energy, scan number, central pixel and detector."
-            " If you are working with the vertical configuration, make sure that the mask is correct."))
+            " If you are working with the vertical configuration, make sure that the mask is correct."
+            "If a detector rotation is needed, you can look at the functions in gwaihir.scripts.handle_sixs_data.py"
+            ))
 
         display(Markdown("## ID01 data (ESRF)"))
         display(Markdown("* Scan number: `11`"))
